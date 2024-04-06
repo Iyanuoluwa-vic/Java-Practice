a7f50d4de8f57852958338cd206dac2bc5ce8748
Victor <adeyeriiyanu@gmail.com>
2024-04-06 18:04:50 -0400

    reading from a file

--- writingToFile.java
+++ writingToFile.java
@@ -1,29 +1,38 @@
 import java.io.PrintWriter;
-import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.io.FileWriter;
-import java.io.IOException;
 import java.io.File;
+import java.io.IOException;
+
 
 
 public class writingToFile {
     public static void main(String[] args) throws IOException  {
+
+        // writing to an existing file
         FileWriter fwrite = new FileWriter("school.txt",true);
         PrintWriter outputFile = new PrintWriter(fwrite);
-        
         outputFile.println("Name");
+        outputFile.close();
 
 
-    
 
+        // writing to a new file
         PrintWriter otherFile = new PrintWriter("tution.txt");
         otherFile.println("James");
         otherFile.println("jdmd");
         otherFile.println("jdmd"); 
         otherFile.println("jdmd");
-
-
         otherFile.close();
-        outputFile.close();
+        File file =new File("school.txt");
+        Scanner inputFile = new Scanner(file);
+
+        while(inputFile.hasNext()){
+            String text = inputFile.nextLine();
+            System.out.println(text);
+        }
+
+        inputFile.close();
 
     
     }
