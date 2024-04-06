64f500faa41b86d68fc3f9e0fe138ce112786938
Victor <adeyeriiyanu@gmail.com>
2024-04-06 15:41:18 -0400

    checking to see if you can get a student loan

--- condtionVariable.java
+++ condtionVariable.java
@@ -11,6 +11,7 @@ public class condtionVariable {
 
         // this is used to caculate Grade of student  base on their score
          int grade =keyboard.nextInt();
+         keyboard.nextLine();
         if(grade >= 90){
             System.out.println("You hade a grade of A" );
         }
@@ -29,6 +30,49 @@ public class condtionVariable {
          else {
               System.out.println("You hade a grade of F" );
         }
+
+
+
+        // To check if you meet getting a student loan
+
+        String Citizen;
+        int CreditScore;
+        int age;
+
+        System.out.println("Where you born in the United State: Enter yes or no");
+        Citizen =keyboard.nextLine();
+
+        while((Citizen.compareTo("yes")!= 0) && (Citizen.compareTo("Yes")!= 0) && (Citizen.compareTo("No")!= 0) && (Citizen.compareTo("no")!= 0) ){
+
+            System.out.println("Incorrect value entered");
+            System.out.println("Where you born in the United State: Enter yes or no");
+            Citizen =keyboard.nextLine();
+        }
+
+        System.out.println("Enter your credit score");
+        CreditScore =keyboard.nextInt();
+
+        System.out.println("Enter your age");
+        age =keyboard.nextInt();
+
+
+
+        if(Citizen.equals( "yes")){
+            if(CreditScore >700){
+                if(age >=18){
+                  System.out.println("You can get a fincial aid");
+
+                }
+            }  
+        
+            else{
+                 System.out.println("You can not get a fincial aid");
+            }
+        }
+        else{
+            System.out.println("You can not get a fincial aid");
+        }
+
 
 
 
